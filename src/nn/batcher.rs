4da2e2909d299c637ab80105b50f8d7721be85ee
Martín Lombardo <to@mlomb.me>
2023-11-15 19:04:14 -0300

    batcher inputs

--- src/nn/batcher.rs
+++ src/nn/batcher.rs
@@ -1,7 +1,8 @@
 // https://github.com/epwalsh/batched-fn/blob/main/src/lib.rs
 
 use flume::{unbounded, Sender};
-use ort::{Session, SessionInputs, SessionOutputs};
+use ndarray::{Array, ArrayD, ArrayViewD, Axis, IxDyn};
+use ort::Session;
 use std::{thread::JoinHandle, time::Duration};
 
 enum Message {
@@ -9,7 +10,7 @@ enum Message {
     Stop,
 
     /// A new batch has been requested
-    Sample(usize, Sender<usize>),
+    Sample(Vec<ArrayD<f32>>, Sender<usize>),
 }
 
 pub struct Batcher {
@@ -25,16 +26,45 @@ impl Batcher {
         let (tx, rx) = unbounded();
 
         let handle = std::thread::spawn(move || {
-            let mut batch_inputs = Vec::with_capacity(max_batch_size);
             let mut batch_txs = Vec::with_capacity(max_batch_size);
+            let mut batch_inputs: Vec<ArrayD<f32>> = session
+                .inputs
+                .into_iter()
+                .map(|input| {
+                    input
+                        .input_type
+                        .tensor_dimensions()
+                        .expect("input tensor with dimensions")
+                        .clone()
+                })
+                .map(|shape| {
+                    assert!(shape[0] == -1, "model must support dynamic batch size");
+                    assert!(shape[1..].iter().all(|&d| d != -1), "only one dynamic");
+
+                    let mut shape = shape
+                        .into_iter()
+                        .map(|d| d as usize)
+                        .collect::<Vec<usize>>();
+                    // reserve size for the biggest batch
+                    shape[0] = max_batch_size;
+                    shape
+                })
+                .map(|shape| ArrayD::zeros(shape))
+                .collect();
 
             let mut i = 0;
 
             // Wait for the first input in the batch
-            while let Ok(Message::Sample(input, result_tx)) = rx.recv() {
+            while let Ok(Message::Sample(inputs, result_tx)) = rx.recv() {
                 // Start building batch
-                batch_inputs.push(input);
                 batch_txs.push(result_tx);
+                batch_inputs
+                    .iter_mut()
+                    .zip(inputs)
+                    .for_each(|(batch_input, input)| {
+                        // Note that this will panic if the input is not the right shape
+                        batch_input.index_axis_mut(Axis(0), 0).assign(&input)
+                    });
 
                 let deadline = std::time::Instant::now() + max_wait_time;
 
@@ -46,10 +76,15 @@ impl Batcher {
                                 // Abort batch
                                 return;
                             }
-                            Message::Sample(input, result_tx) => {
+                            Message::Sample(inputs, result_tx) => {
                                 // Add the input to the batch
-                                batch_inputs.push(input);
                                 batch_txs.push(result_tx);
+                                batch_inputs.iter_mut().zip(inputs).for_each(
+                                    |(batch_input, input)| 
+                                        // Note that this will panic if the input is not the right shape
+                                        batch_input.index_axis_mut(Axis(0), batch_txs.len() - 1).assign(&input)
+                                    ,
+                                );
                             }
                         }
                     } else {
@@ -59,11 +94,10 @@ impl Batcher {
                 }
 
                 // Run the batch
-                println!("Running batch {}: {:?}", i, batch_inputs.len());
+                //println!("Running batch {}: {:?}", i, batch_inputs.len());
                 batch_txs.iter().for_each(|tx| tx.send(i).unwrap());
                 i += 1;
-
-                session.run(input_values);
+                //session.run(ort::inputs![batch_inputs]);
 
                 // Cleanup
                 batch_inputs.clear();
@@ -82,15 +116,12 @@ impl Batcher {
         self.handle.join().unwrap();
     }
 
-    pub fn run<'s>(
-        &self,
-        input_values: impl Into<SessionInputs<'s>>,
-    ) -> Result<SessionOutputs, usize> {
+    pub fn run(&self, inputs: Vec<ArrayD<f32>>) -> usize {
         // Create a channel to get the data back
         let (result_tx, result_rx) = unbounded();
 
         // Send the input to the batch thread
-        self.tx.send(Message::Sample(input, result_tx)).unwrap();
+        self.tx.send(Message::Sample(inputs, result_tx)).unwrap();
 
         // Wait for the result
         result_rx.recv().unwrap()
@@ -100,6 +131,7 @@ impl Batcher {
 #[cfg(test)]
 mod tests {
     use super::Batcher;
+    use ndarray::ArrayD;
     use ort::{download::vision::ImageClassification, Environment, Session, SessionBuilder};
     use std::time::Duration;
 
@@ -107,7 +139,8 @@ mod tests {
         let environment = Environment::builder().build()?.into_arc();
         let session = SessionBuilder::new(&environment)?
             .with_intra_threads(1)?
-            .with_model_downloaded(ImageClassification::MobileNet)?;
+            .with_model_from_memory(include_bytes!("../../tests/test_model.onnx"))?;
+        //.with_model_downloaded(ImageClassification::MobileNet)?;
 
         Ok(session)
     }
@@ -129,8 +162,9 @@ mod tests {
                                 // random
                                 rand::Rng::gen_range(&mut rng, 0..200),
                             ));
-                            let v = batcher.run(key);
-                            println!("{}: {}", key, v);
+                            let inputs = vec![ArrayD::zeros(vec![7, 6, 4])];
+                            let v = batcher.run(inputs);
+                            //println!("{}: {}", key, v);
                         }
                     }
                 });
