3c4af87b9c5e04ad7d2c9ed18338347dcf92d754
Martín Lombardo <to@mlomb.me>
2024-03-20 11:47:50 -0300

    measuing puzzles working

--- notebooks/lib/puzzles.py
+++ notebooks/lib/puzzles.py
@@ -4,16 +4,17 @@ import threading
 import chess
 import chess.engine
 from tqdm import tqdm
+import random
 
 engines: dict[chess.engine.SimpleEngine] = {}
-exit_stack = ExitStack()
 
 def init_engine(engine_cmd: str):
     engine = chess.engine.SimpleEngine.popen_uci(engine_cmd)
-    engines[threading.current_thread()] = exit_stack.enter_context(engine)
+    engines[threading.current_thread()] = engine
 
 
 def solve_puzzle(board: chess.Board, solution: list[chess.Move]) -> bool:
+    board = board.copy()
     solution = solution.copy() # clone
     engine = engines[threading.current_thread()]
 
@@ -24,7 +25,7 @@ def solve_puzzle(board: chess.Board, solution: list[chess.Move]) -> bool:
         # play opponent move
         board.push(opp_move)
 
-        res = engine.play(board, chess.engine.Limit(time=50/1000))
+        res = engine.play(board, chess.engine.Limit(time=100/1000))
 
         if res.move != expected_move:
             # Mate in 1 puzzles in Lichess can have more than one solution
@@ -41,7 +42,7 @@ class PuzzleAccuracy:
         self.puzzles = []
 
         # read and store puzzles
-        with open('/mnt/c/Users/mlomb/Desktop/Tesis/cs-master-thesis/puzzles.csv', 'r') as f:
+        with open('/mnt/c/Users/mlomb/OneDrive/Escritorio/cs-master-thesis/puzzles.csv', 'r') as f:
             for line in f:
                 fen, moves, rating = line.strip().split(',')
                 board = chess.Board(fen)
@@ -49,46 +50,48 @@ class PuzzleAccuracy:
                 rating = int(rating)
 
                 self.puzzles.append((board, moves, rating))
+
+        random.Random(42).shuffle(self.puzzles)
+        self.puzzles = self.puzzles[:40]
     
     def measure(self, engine_cmd: str):
         def f(puzzle):
-            board, moves, rating = puzzle
-            if solve_puzzle(board, moves):
-                return rating
-            else:
-                return -rating
-
-        with ThreadPoolExecutor(max_workers=5, initializer=init_engine, initargs=(engine_cmd,)) as executor:
-            results = list(tqdm(executor.map(f, self.puzzles), total=len(self.puzzles)))
-            
-            print(results)
-
-        return 0
+            board, moves, _ = puzzle
+            return solve_puzzle(board, moves)
 
         ELO_BUCKET_SIZE = 200
         NUM_BUCKETS = 3000 // ELO_BUCKET_SIZE
         buckets_solved = [0] * NUM_BUCKETS
         buckets_total = [0] * NUM_BUCKETS
 
-        for (board, moves, rating) in tqdm(self.puzzles):
-            bucket = rating // ELO_BUCKET_SIZE
-            if bucket >= NUM_BUCKETS: # too high rating
-                continue
+        with ThreadPoolExecutor(max_workers=5, initializer=init_engine, initargs=(engine_cmd,)) as executor:
+            solved = list(tqdm(executor.map(f, self.puzzles), total=len(self.puzzles)))
+            
+            for (_, _, rating), solved in zip(self.puzzles, solved):
+                b = rating // ELO_BUCKET_SIZE
+                if b >= NUM_BUCKETS: # too high rating
+                    continue
 
-            buckets_total[bucket] += 1
+                buckets_total[b] += 1
+                if solved:
+                    buckets_solved[b] += 1
+        
+        # exit all engines
+        global engines
+        for _, engine in engines.items():
+            engine.quit()
+        engines = {}
 
-            if self.solve_puzzle(engine, board, moves):
-                buckets_solved[bucket] += 1
+        result = []
+        for b in range(0, NUM_BUCKETS):
+            if buckets_total[b] > 0:
+                result.append((b * ELO_BUCKET_SIZE, buckets_solved[b] / buckets_total[b]))
 
-        print(buckets_total)
-
-        engine.quit()
-
-        return [s / t if t > 0 else 0 for s, t in zip(buckets_solved, buckets_total)]
+        return result
 
 if __name__ == '__main__':
 
     puzzle_acc = PuzzleAccuracy()
-    res = puzzle_acc.measure('/mnt/c/Users/mlomb/Desktop/Tesis/cs-master-thesis/bot/target/release/bot')
+    res = puzzle_acc.measure('/mnt/c/Users/mlomb/OneDrive/Escritorio/cs-master-thesis/bot/target/release/bot')
     #res = puzzle_acc.measure('/home/mlomb/engines/stockfish-ubuntu-x86-64-avx2')
     print(res)
