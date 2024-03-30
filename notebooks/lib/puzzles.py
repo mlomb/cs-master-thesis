3827b76a40b489e7a0dceaddded58ac21b4f4164
Martín Lombardo <to@mlomb.me>
2024-03-30 03:19:05 -0300

    puzzles by theme too

--- notebooks/lib/puzzles.py
+++ notebooks/lib/puzzles.py
@@ -48,42 +48,45 @@ class PuzzleAccuracy:
         # read and store puzzles
         with open(puzzles_csv, 'r') as f:
             for line in f:
-                fen, moves, rating = line.strip().split(',')
+                fen, moves, rating, themes = line.strip().split(',')
                 board = chess.Board(fen)
                 moves = [chess.Move.from_uci(m) for m in moves.split(' ')]
                 rating = int(rating)
+                themes = themes.split(' ')
                 if rating >= 3000: # rating too high
                     continue
+                
+                bucket = rating // self.elo_bucket_size
+                themes.append('rating' + str(bucket * self.elo_bucket_size) + 'to' + str((bucket + 1) * self.elo_bucket_size - 1))
 
-                self.puzzles.append((board, moves, rating))
+                self.puzzles.append((board, moves, themes))
 
-        random.Random(42).shuffle(self.puzzles)
-        self.puzzles = self.puzzles[:1000]
+        random.Random(42).shuffle(self.puzzles) # puzzles should be already sorted but just in case...
+        #self.puzzles = self.puzzles[:100]
     
     def measure(self, engine_cmd: str | list[str]):
         def f(puzzle):
             board, moves, _ = puzzle
             return solve_puzzle(board, moves)
 
-        NUM_BUCKETS = 3000 // self.elo_bucket_size
-        buckets_solved = [0] * NUM_BUCKETS
-        buckets_total = [0] * NUM_BUCKETS
         correct_moves = 0
         total_moves = 0
+
+        themes_solved = {}
+        themes_total = {}
 
         with ThreadPoolExecutor(max_workers=5, initializer=init_engine, initargs=(engine_cmd,)) as executor:
             results = list(tqdm(executor.map(f, self.puzzles), total=len(self.puzzles)))
             
-            for (_, _, rating), (pz_correct_moves, pz_total_moves) in zip(self.puzzles, results):
-                bucket = rating // self.elo_bucket_size
+            for (_, _, themes), (pz_correct_moves, pz_total_moves) in zip(self.puzzles, results):
                 solved = (pz_correct_moves == pz_total_moves)
 
                 correct_moves += pz_correct_moves
                 total_moves += pz_total_moves
 
-                buckets_total[bucket] += 1
-                if solved:
-                    buckets_solved[bucket] += 1
+                for theme in themes:
+                    themes_total[theme] = themes_total.get(theme, 0) + 1
+                    themes_solved[theme] = themes_solved.get(theme, 0) + (1 if solved else 0)
 
         # exit all engines
         global engines
@@ -92,19 +95,14 @@ class PuzzleAccuracy:
         engines = {}
 
         result = []
-        for b in range(0, NUM_BUCKETS):
-            if buckets_total[b] > 0:
-                result.append((
-                    b * self.elo_bucket_size,
-                    (b+1) * self.elo_bucket_size - 1,
-                    buckets_solved[b] / buckets_total[b]
-                ))
+        for theme in themes_total:
+            result.append((theme, themes_solved[theme] / themes_total[theme]))
 
         return result, (correct_moves / total_moves)
 
 if __name__ == '__main__':
 
-    puzzle_acc = PuzzleAccuracy('/mnt/c/Users/mlomb/Desktop/Tesis/cs-master-thesis/puzzles.csv')
-    a, b = puzzle_acc.measure('/home/mlomb/engines/stockfish-ubuntu-x86-64-avx2')
-    #res = puzzle_acc.measure('/mnt/c/Users/mlomb/OneDrive/Escritorio/cs-master-thesis/bot/target/release/bot')
+    puzzle_acc = PuzzleAccuracy('../data/puzzles.csv')
+    #a, b = puzzle_acc.measure('/home/mlomb/engines/stockfish-ubuntu-x86-64-avx2')
+    a, b = puzzle_acc.measure('/mnt/c/Users/mlomb/Desktop/Tesis/cs-master-thesis/engine/target/release/engine')
     print(a, b)
