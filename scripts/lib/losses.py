857cdefbe98afc7510e7ed8fff6dc6d2435c4768
Martín Lombardo <to@mlomb.me>
2024-09-07 22:13:30 -0300

    fixed losses

--- scripts/lib/losses.py
+++ scripts/lib/losses.py
@@ -1,6 +1,25 @@
 import torch
 
-SCALING = 356.0
+# https://discord.com/channels/435943710472011776/718853716266188890/821010651756363818 (Stockfish Discord)
+in_scaling = 410
+out_scaling = 361
+
+
+class EvalLoss(torch.nn.Module):
+    def __init__(self):
+        super(EvalLoss, self).__init__()
+
+    def forward(self, output, target):
+        # Specific commit before doing a more complex refinement
+        # https://github.com/official-stockfish/nnue-pytorch/blob/50eed1cea1f0c36dd595672005434a99116ff525/model.py#L295-L307
+
+        # CP-space to WDL-space [0, 1]
+        q = (output / out_scaling).sigmoid()
+        p = (target / in_scaling).sigmoid()
+
+        loss = torch.pow(torch.abs(p - q), 2.6).mean()
+
+        return loss
 
 class PQRLoss(torch.nn.Module):
     def __init__(self):
@@ -8,32 +27,17 @@ class PQRLoss(torch.nn.Module):
 
     def forward(self, output, _target):
         output = output.reshape(-1, 3)
-        
-        p = output[:,0] / SCALING
-        q = output[:,1] / SCALING
-        r = output[:,2] / SCALING
-        
-        a = -torch.mean(torch.log(torch.sigmoid(r - q)))
-        b = torch.mean(torch.square(p + q))
 
-        loss = a + b
+        kappa = 1
 
+        p = output[:,0] / out_scaling
+        q = output[:,1] / out_scaling
+        r = output[:,2] / out_scaling
+
+        a = -torch.log(torch.sigmoid(q - r)).mean()
+        b = -torch.log(torch.sigmoid(kappa * (p + q))).mean()
+        c = -torch.log(torch.sigmoid(kappa * (-q - p))).mean()
+
+        loss = a + b + c
+        
         return loss
-
-class EvalLoss(torch.nn.Module):
-    def __init__(self):
-        super(EvalLoss, self).__init__()
-
-    def forward(self, output, target):
-
-        # go from UCI cp to Stockfish's internal engine units
-        # https://github.com/official-stockfish/Stockfish/blob/fb07281f5590bc216ecbacd468aa0d06fdead70c/src/uci.cpp#L341
-        target = target * SCALING / 100.0
-
-        # targets are in CP-space change it to WDL-space [0, 1]
-        wdl_model = torch.sigmoid(output / SCALING)
-        wdl_target = torch.sigmoid(target / SCALING)
-
-        loss = torch.pow(torch.abs(wdl_model - wdl_target), 2.5)
-
-        return loss.mean()
