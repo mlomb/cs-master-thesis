078e945534a3dcb91f044612c1ac38a19d7775e8
Martín Lombardo <to@mlomb.me>
2024-09-20 17:01:38 -0300

    added mobility feature block

--- nn/src/feature_set/blocks/mobility.rs
+++ nn/src/feature_set/blocks/mobility.rs
@@ -17,16 +17,6 @@ impl FeatureBlock for MobilityBlock {
         64 * 6 * 2
     }
 
-    fn requires_refresh(
-        &self,
-        _board: &Board,
-        _mov: &Move,
-        _turn: Color,
-        _perspective: Color,
-    ) -> bool {
-        true
-    }
-
     fn active_features(
         &self,
         board: &Board,
@@ -35,14 +25,6 @@ impl FeatureBlock for MobilityBlock {
         features: &mut Vec<u16>,
         offset: u16,
     ) {
-        //let mut board = board.clone();
-
-        //if perspective == Color::Black {
-        //    // flip board
-        //    board.flip_vertical();
-        //    board.swap_colors();
-        //}
-
         let occupied = board.occupied();
         let unoccupied = !occupied;
 
@@ -68,10 +50,47 @@ impl FeatureBlock for MobilityBlock {
         mut piece_color: Color,
         perspective: Color,
         add_feats: &mut Vec<u16>,
-        _rem_feats: &mut Vec<u16>,
+        rem_feats: &mut Vec<u16>,
         offset: u16,
     ) {
-        panic!("Not implemented");
+        let occupied_prev = board.occupied();
+        let mut occupied_next = board.occupied();
+        occupied_next.set(piece_square, true);
+
+        let unoccupied_prev = !occupied_prev;
+        let unoccupied_next = !occupied_next;
+
+        let accesible_prev = [
+            unoccupied_prev.with(board.by_color(Color::White)),
+            unoccupied_prev.with(board.by_color(Color::Black)),
+        ];
+        let mut accesible_next = [
+            unoccupied_next.with(board.by_color(Color::White)),
+            unoccupied_next.with(board.by_color(Color::Black)),
+        ];
+        accesible_next[piece_color as usize].set(piece_square, true);
+
+        for (piece_square, piece) in board.clone().into_iter() {
+            let attack_prev = attacks::attacks(piece_square, piece, occupied_prev);
+            let attack_next = attacks::attacks(piece_square, piece, occupied_next);
+
+            let mobility_prev = attack_prev & accesible_prev[piece.color.other() as usize];
+            let mobility_next = attack_next & accesible_next[piece.color.other() as usize];
+
+            for to in mobility_prev ^ mobility_next {
+                let to = correct_square(to, perspective);
+                let role = piece.role as u16 - 1;
+                let color = (piece.color != perspective) as u16;
+
+                let index = offset + to as u16 * 12 + role * 2 + color;
+
+                if mobility_next.contains(to) {
+                    add_feats.push(index);
+                } else if mobility_prev.contains(to) {
+                    rem_feats.push(index);
+                }
+            }
+        }
     }
 
     fn features_on_remove(
@@ -81,10 +100,47 @@ impl FeatureBlock for MobilityBlock {
         piece_role: Role,
         piece_color: Color,
         perspective: Color,
-        _add_feats: &mut Vec<u16>,
+        add_feats: &mut Vec<u16>,
         rem_feats: &mut Vec<u16>,
         offset: u16,
     ) {
-        panic!("Not implemented");
+        let occupied_prev = board.occupied();
+        let mut occupied_next = board.occupied();
+        occupied_next.set(piece_square, false);
+
+        let unoccupied_prev = !occupied_prev;
+        let unoccupied_next = !occupied_next;
+
+        let accesible_prev = [
+            unoccupied_prev.with(board.by_color(Color::White)),
+            unoccupied_prev.with(board.by_color(Color::Black)),
+        ];
+        let mut accesible_next = [
+            unoccupied_next.with(board.by_color(Color::White)),
+            unoccupied_next.with(board.by_color(Color::Black)),
+        ];
+        accesible_next[piece_color as usize].set(piece_square, false);
+
+        for (piece_square, piece) in board.clone().into_iter() {
+            let attack_prev = attacks::attacks(piece_square, piece, occupied_prev);
+            let attack_next = attacks::attacks(piece_square, piece, occupied_next);
+
+            let mobility_prev = attack_prev & accesible_prev[piece.color.other() as usize];
+            let mobility_next = attack_next & accesible_next[piece.color.other() as usize];
+
+            for to in mobility_prev ^ mobility_next {
+                let to = correct_square(to, perspective);
+                let role = piece.role as u16 - 1;
+                let color = (piece.color != perspective) as u16;
+
+                let index = offset + to as u16 * 12 + role * 2 + color;
+
+                if mobility_next.contains(to) {
+                    add_feats.push(index);
+                } else if mobility_prev.contains(to) {
+                    rem_feats.push(index);
+                }
+            }
+        }
     }
 }
